dcf6d8499eeb61d5477d8813215cf38287bc6cc4
micouy <szpontaniczny@gmail.com>
2020-09-13 17:49:25 +0200

    Clean up `deps` module

--- src/deps.rs
+++ src/deps.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashMap, path::PathBuf};
+use std::{
+    collections::HashMap,
+    convert::AsRef,
+    path::{Path, PathBuf},
+};
 
 use crate::{
     error::{Error, Result},
@@ -20,26 +24,26 @@ pub struct DependencyGraph {
 impl DependencyGraph {
     pub fn construct(targets: Vec<Target>) -> Result<Self> {
         let mut graph = Graph::new();
-        let mut dep_to_origin_map = HashMap::<PathBuf, _>::new();
-
-        targets.iter().for_each(|target| {
-            target.deps.iter().for_each(|dep| {
+        let mut dep_to_origin_map: HashMap<PathBuf, _> = targets
+            .iter()
+            .map(|target| target.deps.iter())
+            .flatten()
+            .map(|dep| {
                 // initially assume every dependency is a plain file
-                dep_to_origin_map.insert(dep.clone(), DepOrigin::LeafFile);
-            });
-        });
+                (dep.clone(), DepOrigin::LeafFile)
+            })
+            .colect();
 
-        // replace `LeafFile` with `Target` where it's possible
+        // make each output name point to the
+        // corresponding target
         targets
             .into_iter()
-            .map(|target| -> Result<_> {
+            .map(|target| {
                 let id = graph.add_node(target);
+                let returned_slot = dep_to_origin_map
+                    .insert(graph[id].target.clone(), DepOrigin::Target(id));
 
-                // make each output name point to the
-                // corresponding target
-                match dep_to_origin_map
-                    .insert(graph[id].target.clone(), DepOrigin::Target(id))
-                {
+                match returned_slot {
                     Some(DepOrigin::Target(_colliding_id)) =>
                         Err(Error::DuplicateTarget),
                     _ => Ok(()),
@@ -47,35 +51,16 @@ impl DependencyGraph {
             })
             .collect::<Result<()>>()?;
 
+        // add edges from each target to its dependencies
         graph
             .graph()
             .node_indices()
-            .map(|id| {
-                // add edges from each target to its dependencies
-
-                let deps_indices = graph[id]
-                    .deps
-                    .iter()
-                    .filter_map(|dep| {
-                        let origin =
-                            dep_to_origin_map.get(dep).expect("no dependency");
-
-                        match origin {
-                            DepOrigin::Target(ix) => Some(*ix),
-                            DepOrigin::LeafFile => None,
-                        }
-                    })
-                    .collect::<Vec<NodeIndex>>(); // collecting in order to drop the reference to the graph
-
-                deps_indices
-                    .iter()
-                    .map(|dep_id| {
-                        graph
-                            .add_edge(id, *dep_id, ())
-                            .map(|_| ())
-                            .map_err(|_| Error::DependencyCycle)
-                    })
-                    .collect::<Result<_>>()
+            .map(|target_id| {
+                util::connect_target_to_deps(
+                    &mut graph,
+                    &dep_to_origin_map,
+                    target_id,
+                )
             })
             .collect::<Result<_>>()?;
 
@@ -89,17 +74,51 @@ impl DependencyGraph {
 mod util {
     use std::collections::{HashMap, VecDeque};
 
+    use super::*;
+
     use daggy::{petgraph, *};
     use petgraph::prelude::{Direction, EdgeRef, Graph};
 
-    pub fn generate_depth_map<N, E>(
+    pub(super) fn connect_target_to_deps(
+        graph: &mut Dag<Target, ()>,
+        dep_to_origin_map: &HashMap<PathBuf, DepOrigin>,
+        target_id: NodeIndex,
+    ) -> Result<()> {
+        // collecting indices to be able to drop
+        // the reference to the graph
+        let deps_ids = graph[target_id]
+            .deps
+            .iter()
+            .filter_map(|dep| {
+                let origin = dep_to_origin_map.get(dep).unwrap(); // unreachable
+
+                match origin {
+                    DepOrigin::Target(ix) => Some(*ix),
+                    DepOrigin::LeafFile => None,
+                }
+            })
+            .collect::<Vec<NodeIndex>>();
+
+        // add edges
+        deps_ids
+            .iter()
+            .map(|dep_id| {
+                graph
+                    .add_edge(target_id, *dep_id, ())
+                    .map(|_| ())
+                    .map_err(|_| Error::DependencyCycle)
+            })
+            .collect::<Result<_>>()
+    }
+
+    pub(super) fn generate_depth_map<N, E>(
         graph: &Graph<N, E>,
-        target: NodeIndex,
+        target_id: NodeIndex,
     ) -> HashMap<NodeIndex, usize> {
         let mut depth_map: HashMap<NodeIndex, usize> = HashMap::new();
         let mut current_depth = 0;
         let mut queue: VecDeque<Vec<_>> = VecDeque::new();
-        queue.push_front(vec![target]);
+        queue.push_front(vec![target_id]);
 
         while let Some(level) = queue.pop_front() {
             if level.is_empty() {
